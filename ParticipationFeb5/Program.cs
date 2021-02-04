1424fe82a8631da9198ad66783b731e1ec5f184f
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-04 17:47:54 -0600

    Added input and output

--- ParticipationFeb5/Program.cs
+++ ParticipationFeb5/Program.cs
@@ -6,7 +6,20 @@ namespace ParticipationFeb5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double number = 7.777; //local variable 
+
+            Console.WriteLine("Enter in 3 different number:");
+            Console.WriteLine("Enter in the first number >>");
+            double val1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter in the second number >>");
+            double val2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter in the third number >>");
+            double val3 = Convert.ToDouble(Console.ReadLine());
+
+            double sumTotal = val1 + val2 + val3;
+            Console.WriteLine("Sum Total: " + sumTotal);
+            double multiTotal = sumTotal * number;
+            Console.WriteLine("Multiply Total: " + multiTotal);
         }
     }
 }
