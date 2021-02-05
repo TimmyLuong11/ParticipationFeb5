1f2cd2824d456e3c8b08b0a6df17ba72d99f0228
Timothy Luong <Timothy.Luong@ou.edu>
2021-02-04 18:15:14 -0600

    Added comment explaing what each seciton is and what it does

--- ParticipationFeb5/Program.cs
+++ ParticipationFeb5/Program.cs
@@ -6,19 +6,25 @@ namespace ParticipationFeb5
     {
         static void Main(string[] args)
         {
-            const double number = 7.777; //local variable 
+            //Declaring Variables
+            double val1, val2, val3, sumTotal, multiTotal;
+            const double number = 7.777; //local constant variable 
 
+            //Output information to console to gather user input and taking the user input assiging to variables
             Console.WriteLine("Enter in 3 different number:");
             Console.WriteLine("Enter in the first number >>");
-            double val1 = Convert.ToDouble(Console.ReadLine());
+            val1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter in the second number >>");
-            double val2 = Convert.ToDouble(Console.ReadLine());
+            val2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter in the third number >>");
-            double val3 = Convert.ToDouble(Console.ReadLine());
+            val3 = Convert.ToDouble(Console.ReadLine());
 
-            double sumTotal = val1 + val2 + val3;
+            //Adding the user input and outputting to console
+            sumTotal = val1 + val2 + val3;
             Console.WriteLine("Sum Total: " + sumTotal);
-            double multiTotal = sumTotal * number;
+
+            //Multiplying the user input with a const and outputting to console
+            multiTotal = sumTotal * number;
             Console.WriteLine("Multiply Total: " + multiTotal);
         }
     }
